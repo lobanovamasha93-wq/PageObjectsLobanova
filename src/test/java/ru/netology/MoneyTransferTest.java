97f781438a0cd2e0f87609fea9eabb47de3c0ab9
Maria Lobanova <marusa.lobanova2015@yandex.ru>
2026-04-09 17:08:06 +0400

    Implement Page Object pattern for Money Transfer project

---
+++ src/test/java/ru/netology/MoneyTransferTest.java
@@ -0,0 +1,50 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        var dashboardPage = new DashboardPage();
+
+        int balanceFirstBefore = dashboardPage.getCardBalance(0);
+        int balanceSecondBefore = dashboardPage.getCardBalance(1);
+        int amount = 500;
+
+        var transferPage = dashboardPage.selectCardToTransfer(0);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), DataHelper.getSecondCardInfo());
+
+        int balanceFirstAfter = dashboardPage.getCardBalance(0);
+        int balanceSecondAfter = dashboardPage.getCardBalance(1);
+
+        assertEquals(balanceFirstBefore + amount, balanceFirstAfter);
+        assertEquals(balanceSecondBefore - amount, balanceSecondAfter);
+    }
+
+    @Test
+    void shouldFindBugWhenTransferMoreThanBalance() {
+        var dashboardPage = new DashboardPage();
+
+        int balanceFirstBefore = dashboardPage.getCardBalance(0);
+        int balanceSecondBefore = dashboardPage.getCardBalance(1);
+        int amount = balanceSecondBefore + 1000;
+
+        var transferPage = dashboardPage.selectCardToTransfer(0);
+        transferPage.makeTransfer(String.valueOf(amount), DataHelper.getSecondCardInfo());
+    }
+}
---
+++ src/test/java/ru/netology/VerificationPage.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    public VerificationPage() {
+        $("[data-test-id=code] input").shouldBe(visible);
+    }
+
+    public void validVerify(String verificationCode) {
+        $("[data-test-id=code] input").setValue(verificationCode);
+        $("[data-test-id=action-verify]").click();
+        new DashboardPage();
+    }
+}
